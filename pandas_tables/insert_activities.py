794c94dfa2450c1e6d15ff417d3b5c25d13d3a9f
eriksundstrooom <103208010+eriksundstrooom@users.noreply.github.com>
2024-10-08 10:50:27 +0200

    changes to insert_activities

--- pandas_tables/insert_activities.py
+++ pandas_tables/insert_activities.py
@@ -1,9 +1,24 @@
-from DbConnector import DbConnector
+import pandas as pd
 import mysql.connector
+import sys
+import os
 
-from pandas_tables.activity_tables import activity_pandas
+# Add the parent directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from DbConnector import DbConnector
 
-def insert_users(users_pandas):
+# Define the path to your CSV file
+csv_file_path = "/Users/eriksundstrom/store-distribuerte-datamengder/cleaned_tables/activity_data.csv"
+
+# Read the CSV file into a pandas DataFrame
+activity_pandas = pd.read_csv(csv_file_path)
+
+# Convert 'start_date_time' and 'end_date_time' to datetime objects
+activity_pandas['start_date_time'] = pd.to_datetime(activity_pandas['start_date_time'])
+activity_pandas['end_date_time'] = pd.to_datetime(activity_pandas['end_date_time'])
+
+
+def insert_activities(activity_pandas):
     db = DbConnector()  # Establish a connection to the database
     cursor = db.cursor
     
@@ -14,7 +29,7 @@ def insert_users(users_pandas):
     """  # Using ON DUPLICATE KEY to avoid inserting duplicate users
     
     # Iterate over the users DataFrame and insert each user into the database
-    for index, row in users_pandas.iterrows():
+    for index, row in activity_pandas.iterrows():
         id = row['id']
         user_id = row['user_id']
         transportation_mode = row['transportation_mode']
@@ -30,4 +45,4 @@ def insert_users(users_pandas):
     db.close()  # Close the connection
 
 # Call the function to insert users
-insert_users(activity_pandas)
+insert_activities(activity_pandas)
